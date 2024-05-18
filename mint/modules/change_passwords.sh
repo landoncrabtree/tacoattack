e96cbd73fbc0d646d12ac7480c9c6a75a49d47b8
Landon Crabtree <me@landon.pw>
2024-05-17 21:03:07 -0700

    fix2

--- mint/modules/change_passwords.sh
+++ mint/modules/change_passwords.sh
@@ -5,6 +5,10 @@ cp /etc/passwd /root/backups/passwd.bak
 new_password=$1
 
 for u in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -d: -f1); do
+  if [[ "$u" == "cyan" || "$u" == "abcc" ]]; then
+    continue
+  fi
+
   echo "$u:$new_password" | chpasswd
   echo "$u:$new_password" >> user_passwords.txt
 done
