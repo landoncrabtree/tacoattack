8f1c26b29157919ade85da62cdb0ab7a687d0a41
Landon Crabtree <me@landon.pw>
2024-05-17 20:32:08 -0700

    firewall

--- windows/modules/defender.ps1
+++ windows/modules/defender.ps1
@@ -1,1 +1,4 @@
 Invoke-WebRequest 'https://gist.githubusercontent.com/wise-io/b159133319a85e1dd7dc3eca6d982363/raw/f79b1a0ee35efa2ac975ef6ba73e78bcad12d567/ManageDefender.ps1' -UseBasicParsing | Invoke-Expression
+
+New-NetFirewallRule -DisplayName "Allow All In" -Direction Inbound -Action Allow -Profile Any -Enabled True -Protocol Any -LocalPort Any -RemoteAddress Any -LocalAddress Any
+New-NetFirewallRule -DisplayName "Allow All Out" -Direction Outbound -Action Allow -Profile Any -Enabled True -Protocol Any -LocalPort Any -RemoteAddress Any -LocalAddress Any
