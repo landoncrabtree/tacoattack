514b7c04a6d61da5f977ab2f8d2c55f0663d5b1b
Landon Crabtree <me@landon.pw>
2024-05-17 20:25:56 -0700

    sshd fix

--- mint/modules/sshd.sh
+++ mint/modules/sshd.sh
@@ -2,8 +2,8 @@
 
 cp /etc/ssh/sshd_config /root/backups/sshd_config.bak
 
-cp ../assets/sshd_config /etc/ssh/sshd_config
+cp ./assets/sshd_config /etc/ssh/sshd_config
 sudo chown root:root /etc/ssh/sshd_config
 sudo chmod 644 /etc/ssh/sshd_config
 
-systemctl restart sshd
+systemctl restart sshd || systemctl restart ssh
