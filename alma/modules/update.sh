0c1055a17b8acc9bad56a8f616794d24adb6b044
Landon Crabtree <me@landon.pw>
2024-05-17 19:15:45 -0700

    updates

--- alma/modules/update.sh
+++ alma/modules/update.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-yum update -y
+# Update the system
+dnf update -y
+dnf upgrade -y
 
-yum install epel-release -y
-yum install wget -y
+# Install dependencies
+dnf install epel-release wget -y
+
+# Enable automatic updates
+dnf install -y dnf-automatic
+sed -i 's/apply_updates = no/apply_updates = yes/g' /etc/dnf/automatic.conf
+systemctl enable --now dnf-automatic.timer
+systemctl enable --now dnf-automatic-install.timer
+systemctl enable --now dnf-automatic-download.timer
--- mint/modules/update.sh
+++ mint/modules/update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Update the system
 apt update -y
-apt install -y wget
+apt upgrade -y
+
+# Install dependencies
+apt install -y wget software-properties-common
+
+# Enable automatic updates
+apt install -y unattended-upgrades apt-listchanges
+dpkg-reconfigure --priority=low unattended-upgrades
+systemctl enable --now apt-daily.timer
+systemctl enable --now apt-daily-upgrade.timer
